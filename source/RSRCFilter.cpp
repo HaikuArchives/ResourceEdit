c535d09539c7d20b840bdae186433017f8f480b7
Raheem Idowu <abdurraheemidowu@gmail.com>
2020-01-06 19:19:19 +0000

    rejects .o files, case insensitive, gcc2 support

--- source/RSRCFilter.cpp
+++ source/RSRCFilter.cpp
@@ -1,14 +1,14 @@
 #include "RSRCFilter.h"
 
 #include <MimeType.h>
-#include <string.h>
+#include <string>
 
 static const char* supported_types[] = {
-	"application/x-vnd.Be-elfexecutable",
+	"application/x-vnd.be-elfexecutable",
 	"application/octet-stream",
 	NULL
 };
-	
+
 bool
 RSRCFilter::Filter(const entry_ref* ref, BNode* node, struct stat_beos* st,
 	const char* filetype)
@@ -18,19 +18,21 @@ RSRCFilter::Filter(const entry_ref* ref, BNode* node, struct stat_beos* st,
 		return false;
 	if (entry.IsDirectory())
 		return true;
-	
+
 	entry_ref entryRef;
 	if (entry.GetRef(&entryRef) != B_OK)
 		return false;
-	
+
 	BMimeType mimeType;
 	if (BMimeType::GuessMimeType(&entryRef, &mimeType) != B_OK)
 		return false;
 
-	for (int i = 0; supported_types[i] != nullptr; i++) {
-		if (strcmp(mimeType.Type(), supported_types[i]) == 0)
-			return true;
+	for (int i = 0; supported_types[i] != NULL; i++) {
+		if (strcasecmp(mimeType.Type(), supported_types[i]) == 0)
+			if (entryRef.name[strlen(entryRef.name)-1] != 'o'
+				&& entryRef.name[strlen(entryRef.name)-2] != '.')
+				return true;
 	}
-	
+
 	return false;
 }
