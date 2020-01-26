d1a1e15edfcf4e9b2f9f4510fe37b37ad3214155
Abdur-Raheem Idowu <abdurraheemidowu@gmail.com>
2020-01-26 22:17:46 +0000

    logic improvements - thanks owen

--- source/RSRCFilter.cpp
+++ source/RSRCFilter.cpp
@@ -11,11 +11,6 @@
 
 #include <compat/sys/stat.h>
 
-static const char* supported_types[] = {
-	"application/x-vnd.be-elfexecutable",
-	NULL
-};
-
 bool
 RSRCFilter::Filter(const entry_ref* ref, BNode* node, struct stat_beos* st,
 	const char* filetype)
@@ -33,36 +28,28 @@ RSRCFilter::Filter(const entry_ref* ref, BNode* node, struct stat_beos* st,
 	char mimeType[B_MIME_TYPE_LENGTH];
 
 	//Check if ref is a symlink
-	bool isLink = S_ISLNK(st->st_mode);
-	if (isLink) {
+	if (S_ISLNK(st->st_mode)) {
 		BNode traversedNode(&entry);
-		BNodeInfo * nodeInfo = new BNodeInfo(&traversedNode);
-		if (nodeInfo->GetType(mimeType) != B_OK) {
-			delete nodeInfo;
+		BNodeInfo nodeInfo(&traversedNode);
+		if (nodeInfo.GetType(mimeType) != B_OK)
 			return false;
-		} else {
-			delete nodeInfo;
-			filetype = mimeType;
-		}
+		filetype = mimeType;
 	}
 
 	//All files of non-BFS drives report as octet-stream or empty
-	if (strcasecmp(filetype, "application/octet-stream") == 0
-		|| filetype[0] == '\0') {
+	if ((strcasecmp(filetype, "application/octet-stream") == 0
+		|| filetype[0] == '\0') && filetype) {
 		BMimeType mimeType;
 		if (BMimeType::GuessMimeType(&entryRef, &mimeType) != B_OK)
 			return false;
 		filetype = mimeType.Type();
 	}
 
-	for (int i = 0; supported_types[i] != NULL; i++) {
-		if (strcasecmp(filetype, supported_types[i]) != 0)
-			continue;
-		if (strlen(entryRef.name) < 3)
-			return true;
-		const char* secondToLast = &entryRef.name[strlen(entryRef.name) - 2];
-			return strcmp(secondToLast, ".o") != 0;
-	}
 
-	return false;
+	if (strcasecmp(filetype, "application/x-vnd.be-elfexecutable") != 0)
+		return false;
+	if (strlen(entryRef.name) < 3)
+		return true;
+	const char* secondToLast = &entryRef.name[strlen(entryRef.name) - 2];
+	return strcmp(secondToLast, ".o") != 0;
 }
